1ad8b1e8a185e3a5087c25f38ed4a55dd9088db7
Bankole_Adefemi <bankoledavidadefemi@gmail.com>
2020-09-06 20:23:05 +0100

    few corrections

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-console.log('connected');
-
-//Selecting all the requiredelements and classes and assigning them to variables
+//Selecting all the required elements and classes and assigning them to variables
 let img1 = document.querySelector('.img1');
 let img2 = document.querySelector('.img2');
 let head = document.querySelector('h1')
@@ -17,9 +15,10 @@ img1.setAttribute('src', count);
 //For the 2nd dice
 let dice2 = Math.random()*6;
 dice2 = Math.floor(dice2)+1;
-count2 = "images/"+"dice"+dice2+".png";
+count2 = `images/dice${dice2}.png`;
 
 img2.setAttribute('src', count2);
+
 
 
 //telling who wins with the higher dice number using tenary operators
