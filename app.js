69daa02d97f17e648c7ee29838310f938995d849
Bankole_Adefemi <bankoledavidadefemi@gmail.com>
2020-12-06 23:07:24 +0100

    new edits

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
 //Selecting all the required elements and classes and assigning them to variables
 let img1 = document.querySelector('.img1');
 let img2 = document.querySelector('.img2');
-let head = document.querySelector('h1')
+let heading = document.querySelector('h1')
 
 //For the 1st dice
+
 let dice1 = Math.random()*6;
 dice1 = Math.floor(dice1)+1;
 count = `images/dice${dice1}.png`
@@ -14,6 +15,7 @@ img1.setAttribute('src', count);
 
 
 //For the 2nd dice to be rolled
+
 let dice2 = Math.random()*6;
 dice2 = Math.floor(dice2)+1;
 count2 = `images/dice${dice2}.png`;
@@ -22,7 +24,7 @@ img2.setAttribute('src', count2);
 
 
 //telling who wins with the higher dice number using tenary operators
-dice1>dice2 ? head.innerHTML = "Player 1 Wins 🚩":
-dice2>dice1 ? head.innerHTML = "Player 2 Wins 🚩":
-head.innerHTML = "Draw";
+dice1>dice2 ? heading.innerHTML = "Player 1 Wins 🚩":
+dice2>dice1 ? heading.innerHTML = "Player 2 Wins 🚩":
+heading.innerHTML = "Draw";
 
